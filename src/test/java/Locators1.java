7c233ed87ab31d14d985bdad599e74c72cdd845b
Mahesh-Babu789 <mahesh.kuraganti@zemosolabs.com>
2023-05-09 10:26:07 +0530

    Added new method to get password dynamically

--- src/test/java/Locators1.java
+++ src/test/java/Locators1.java
@@ -13,10 +13,11 @@ public class Locators1 {
         WebDriver driver = new ChromeDriver();
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        String myPassword = getPassword(driver);
         driver.get("https://rahulshettyacademy.com/locatorspractice/");
         driver.manage().window().maximize();
         driver.findElement(By.id("inputUsername")).sendKeys("Mahesh");
-        driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.name("inputPassword")).sendKeys(myPassword);
         Thread.sleep(2000);
         driver.findElement(By.className("signInBtn")).click();
         Thread.sleep(2000);
@@ -26,6 +27,18 @@ public class Locators1 {
         Thread.sleep(1000);
         Assert.assertEquals(driver.findElement(By.cssSelector(".form h1")).getText(), "Sign in");
         driver.close();
+    }
+
+    public static String getPassword(WebDriver driver) throws InterruptedException {
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        driver.manage().window().maximize();
+        driver.findElement(By.linkText("Forgot your password?")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+        String passwordText = driver.findElement(By.cssSelector("form p")).getText();
+        String[] passwordArray = passwordText.split("'");
+        String myPassword = passwordArray[1].split("'")[0];
+        return myPassword;
     }
 
 }
